08537917a4e8574ad74c02841cbd852f710f792b
David Cian <cian_david@outlook.com>
2020-09-22 16:11:30 +0200

    Add model and wrapper

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from models import PredNet, simple_decoder, Wrapper
+
 # Main parameters
 im_dims     = (64, 64, 3)                                           # input images are 64 by 64 pixels, with 3 color channels (RGB)
 decode_crit = 'last_frame'
@@ -12,3 +14,9 @@ noise_lvl   = {'recons': 0.9,     'decode': 1e-5,  'sqm': None}
 init_lr     = {'recons': 5e-4,    'decode': 1e-5,  'sqm': None}     # initial learning rate; tune this first
 do_best_lr  = {'recons': False,   'decode': False, 'sqm': None}     # whether to run find_best_lr to initialize the learning rate
 do_run      = {'recons': True,    'decode': True,  'sqm': True}
+
+# Model and wrapper
+model, name = PredNet((im_dims[-1], 32, 64, 128), (im_dims[-1], 32, 64, 128)), 'prednet2'
+recons      = None
+decoder     = simple_decoder()
+wrapper     = Wrapper(model, recons, decoder, 0.0, decode_crit, 0, name)
