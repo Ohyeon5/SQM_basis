1633976021b45a8774df7cd45ac63dcd31d27cda
David Cian <cian_david@outlook.com>
2020-10-05 04:15:53 +0200

    Implement basic wrapper

--- models.py
+++ models.py
@@ -1,5 +1,26 @@
 import torch
 
 class Wrapper(torch.nn.module):
-  def __init__(self):
-    None
+  """Wrap a convolutional module, an encoder and a decoder
+
+  Parameters
+  ----------
+  conv_module : torch.nn.Module
+      The convolutional module
+  encoder_module : torch.nn.Module
+      The encoder module
+  decoder_module : torch.nn.Module
+      The decoder module
+      
+  """
+
+  def __init__(self, conv_module, encoder_module, decoder_module):
+    super(Wrapper, self).__init__()
+    self.conv_module = conv_module
+    self.encoder_module = encoder_module
+    self.decoder_module = decoder_module
+
+  def forward(self, x):
+    x = self.conv_module(x)
+    x = self.encoder_module(x)
+    x = self.decoder_module(x)
