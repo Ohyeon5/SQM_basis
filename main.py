40f4bbf8fbcbe8f3df9062c8b6a22aeb49495851
David Cian <cian_david@outlook.com>
2020-10-06 17:37:19 +0200

    Add main script docstring

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ the model labels the sequence with a hand gesture
 2) L/R vernier discrimination: based on a sequence of images containing a vernier,
 the model labels the sequence as a left or right vernier
 """
+
