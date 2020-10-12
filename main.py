e50331f4f67ee93239454b3da930256487f93fea
David Cian <cian_david@outlook.com>
2020-10-12 23:45:44 +0200

    Add wrapped model

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import argparse
 import torch
 from train_hand_gesture_classifier import train_hand_gesture_classifier
 from train_LR_vernier_classifier import train_LR_vernier_classifier
+from models import Wrapper
+from SQM_discreteness.models import Primary_conv3D, ConvLSTM_disc_low, ConvLSTM_disc_high, FF_classifier
 
 arg_parser = argparse.ArgumentParser(description='Train a deep learning model for various tasks')
 arg_parser.add_argument('--n-epochs', type=int)
@@ -22,8 +24,8 @@ arg_parser.add_argument('--batches-per-epoch', type=int)
 command_line_args = arg_parser.parse_args()
 
 do_train_hand_gesture_classifier = True
-do_train_LR_vernier_classifier = True
-model = None # TODO replace model
+do_train_LR_vernier_classifier = False
+model = Wrapper(Primary_conv3D(), ConvLSTM_disc_low(4), FF_classifier(10, 15)) # TODO change in_channels and number of classes
 n_epochs = command_line_args.n_epochs
 batch_size = command_line_args.batch_size
 batches_per_epoch = command_line_args.batches_per_epoch
