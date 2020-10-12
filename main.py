6ed171efa1d6c4d639274032eae20c69df696859
David Cian <cian_david@outlook.com>
2020-10-12 05:42:38 +0200

    Implement command line interface

--- main.py
+++ main.py
@@ -9,20 +9,29 @@ the model labels the sequence with a hand gesture
 the model labels the sequence as a left or right vernier
 """
 
+import argparse
 import torch
 from train_hand_gesture_classifier import train_hand_gesture_classifier
 from train_LR_vernier_classifier import train_LR_vernier_classifier
 
+arg_parser = argparse.ArgumentParser(description='Train a deep learning model for various tasks')
+arg_parser.add_argument('--n-epochs', type=int)
+arg_parser.add_argument('--batch-size', type=int)
+arg_parser.add_argument('--batches-per-epoch', type=int)
+
+command_line_args = arg_parser.parse_args()
+
 do_train_hand_gesture_classifier = True
 do_train_LR_vernier_classifier = True
 model = None # TODO replace model
-n_epochs = None # TODO replace n_epochs
-batch_size = None # TODO replace batch_size
-batches_per_epoch = None # TODO replace batches_per_epoch
+n_epochs = command_line_args.n_epochs
+batch_size = command_line_args.batch_size
+batches_per_epoch = command_line_args.batches_per_epoch
 
 if (do_train_hand_gesture_classifier):
   optimizer = torch.optim.Adam(model.parameters())
   train_hand_gesture_classifier(model, optimizer, n_epochs, batch_size, batches_per_epoch)
 
 if (do_train_LR_vernier_classifier):
-  train_LR_vernier_classifier(model, n_epochs)
+  optimizer = torch.optim.Adam(model.parameters())
+  train_LR_vernier_classifier(model, optimizer, n_epochs, batch_size, batches_per_epoch)
