4915c065fbb89eb8edf5ef4595df3dc7b97010d2
David Cian <cian_david@outlook.com>
2020-11-18 13:21:09 +0100

    Fix training

--- run_config_1.bat
+++ run_config_1.bat
@@ -1,1 +1,1 @@
-python main.py --n-epochs 1 --batch-size 64 --training-data-path "D:/sqm_data/train_hdf52.h5"
+python main.py --n-epochs 10 --batch-size 64 --training-data-path "D:/sqm_data/train_hdf52.h5"
